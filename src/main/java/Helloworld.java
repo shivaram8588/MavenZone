9f111df13a054eb29b0efa912c6699997cc1bbe9
shivaram8588 <65568878+shivaram8588@users.noreply.github.com>
2021-05-01 22:47:55 -0400

    Update Helloworld.java

--- src/main/java/Helloworld.java
+++ src/main/java/Helloworld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
-        public static void main(string[] args{
+        public static void main(string[] args){
                 System.out.println("Welcome to world of Maven");
         }
 }
